d4a0c472799a01a81341c27266920c79c6bc9553
Rahulshahare <rahulshahare@gmail.com>
2020-06-28 15:46:23 +0530

    TextField Widgets are added

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -40,7 +40,7 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
+            /* TODO: Add TextField widgets (101) - Done */
             // [Name]
             TextField(
               decoration: InputDecoration(
