39c01c1b67493848cffc8e2f3f28d25ee9b156cf
Rahulshahare <rahulshahare@gmail.com>
2020-06-28 15:37:43 +0530

    SizedBox works as spacer

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -48,6 +48,7 @@ class _LoginPageState extends State<LoginPage> {
                 labelText: 'Username',
               ),
             ),
+            SizedBox(height: 10.0),
             TextField(
               decoration: InputDecoration(
                 filled: true,
